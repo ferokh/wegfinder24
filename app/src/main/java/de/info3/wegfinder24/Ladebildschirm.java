6024d8d67d7502ccae03a0aa851368856706effa
ferokh <nico@mail-michel.com>
2022-12-05 10:42:03 +0100

    initial commit

---
+++ app/src/main/java/de/info3/wegfinder24/Ladebildschirm.java
@@ -0,0 +1,26 @@
+package de.info3.wegfinder24;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    public final int LOAD_TIME = 750;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Eingabe.class);
+                startActivity(intent);
+            }
+        },LOAD_TIME);
+    }
+}
