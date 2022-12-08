d7df58db1184d502fc07a3fe7bfc5e08007f5708
gojo1021 <johannes.goetz99@gmail.com>
2022-12-08 18:05:28 +0100

    Versuch die 100000000000

--- app/src/main/java/de/info3/wegfinder24/EingabeActivity.java
+++ app/src/main/java/de/info3/wegfinder24/EingabeActivity.java
@@ -11,6 +11,26 @@ import com.mapbox.mapboxsdk.maps.Style;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.location.LocationComponent;
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
+import com.mapbox.mapboxsdk.location.modes.RenderMode;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+
+import java.util.List;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
@@ -56,6 +76,8 @@ public class EingabeActivity extends AppCompatActivity {
 
     public final static String MAPBOX_TOKEN ="pk.eyJ1IjoiYnJ1ZWNrdGkiLCJhIjoiY2xiYzBjNHI4MGI2NTNvcGV3OWI5aTJjciJ9.-ZRC7YTlnbSefuqY1u0TZg";
 
+    private PermissionsManager permissionsManager;
+    private MapboxMap mapboxMap;
     private MapView mapView;
 
 
@@ -67,7 +89,7 @@ public class EingabeActivity extends AppCompatActivity {
         Mapbox.getInstance(this, MAPBOX_TOKEN);
         setContentView(R.layout.activity_eingabe);
 
-        /*ImageButton btnOpenVariante = this.findViewById(R.id.btnArrow);
+        ImageButton btnOpenVariante = this.findViewById(R.id.btnArrow);
 
          btnOpenVariante.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -75,11 +97,8 @@ public class EingabeActivity extends AppCompatActivity {
                 Intent intent = new Intent(EingabeActivity.this, WartebildschirmActivity.class);
                 startActivity(intent);
             }
-        });*/
-
-
-
-        mapView = findViewById(R.id.map);
+        });
+         mapView = findViewById(R.id.map);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(new OnMapReadyCallback() {
             @Override
@@ -89,24 +108,69 @@ public class EingabeActivity extends AppCompatActivity {
                     public void onStyleLoaded(@NonNull Style style) {}
                 });
             }
-
         });
+    }
+    @SuppressWarnings( {"MissingPermission"})
+    private void enableLocationComponent(@NonNull Style loadedMapStyle) {
+// Check if permissions are enabled and if not request
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
 
+            LocationComponent locationComponent = mapboxMap.getLocationComponent();
+            locationComponent.activateLocationComponent(
+                    LocationComponentActivationOptions.builder(this, loadedMapStyle).build());
+            locationComponent.setLocationComponentEnabled(true);
+            locationComponent.setCameraMode(CameraMode.TRACKING);
+            locationComponent.setRenderMode(RenderMode.COMPASS);
+        } else {
+            permissionsManager = new PermissionsManager((PermissionsListener) this);
+            permissionsManager.requestLocationPermissions(this);
+        }
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
-        mapView.onResume();
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
+    /*@Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+        Toast.makeText(this, R.string.user_location_permission_explanation, Toast.LENGTH_LONG).show();
+    }
 
     @Override
+    public void onPermissionResult(boolean granted) {
+        if (granted) {
+            mapboxMap.getStyle(new Style.OnStyleLoaded() {
+                @Override
+                public void onStyleLoaded(@NonNull Style style) {
+                    enableLocationComponent(style);
+                }
+            });
+        } else {
+            Toast.makeText(this, R.string.user_location_permission_not_granted, Toast.LENGTH_LONG).show();
+            finish();
+        }
+    } */
+
+    @Override
+    @SuppressWarnings( {"MissingPermission"})
     protected void onStart() {
         super.onStart();
         mapView.onStart();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
 
     @Override
     protected void onStop() {
@@ -115,15 +179,9 @@ public class EingabeActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onPause() {
-        super.onPause();
-        mapView.onPause();
-    }
-
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        mapView.onLowMemory();
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
     }
 
     @Override
@@ -133,127 +191,9 @@ public class EingabeActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        mapView.onSaveInstanceState(outState);
-    }
-
-
-
-
-
-   /* public final static String MAPBOX_TOKEN="pk.eyJ1IjoiYnJ1ZWNrdGkiLCJhIjoiY2xiYzBjNHI4MGI2NTNvcGV3OWI5aTJjciJ9.-ZRC7YTlnbSefuqY1u0TZg";
-
-    private LocationManager locationManager;
-    MapView mapView;
-    MapboxMap map;
-
-
-
-    @SuppressLint("MissingPermission")
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        Mapbox.getInstance(this, MAPBOX_TOKEN);
-        setContentView(R.layout.activity_eingabe);
-        initView();
-        initPermissions();
-        //über den ArrowButton die Variate Activity öffnen
-        ImageButton btnOpenVariante = this.findViewById(R.id.btnArrow);
-
-        btnOpenVariante.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(EingabeActivity.this, WartebildschirmActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
-
-
-
-
-
-
-
-    protected void initView(){
-        mapView=(MapView) findViewById(R.id.map);
-    }
-
-    protected void initPermissions() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            updateLocation();
-        } else {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
-            }
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode==0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
-            updateLocation();
-        }
-    }
-
-    @SuppressLint("MissingPermission")
-    protected void updateLocation(){
-        mapView.getMapAsync(this::onMapReady);
-    }
-
-    protected void onMapReady(MapboxMap map) {
-        this.map=map;
-        map.setStyle(Style.SATELLITE_STREETS,this::onStyleReady);
-    }
-
-    @SuppressLint("MissingPermission")
-    protected void onStyleReady(Style style) {
-       LocationComponent locationComponent= map.getLocationComponent();
-       locationComponent.activateLocationComponent(LocationComponentActivationOptions.builder(this, style).build());
-       locationComponent.setLocationComponentEnabled(true);
-       locationComponent.setCameraMode(CameraMode.TRACKING);
-       locationComponent.zoomWhileTracking(10,3000);
-       locationComponent.setRenderMode(RenderMode.COMPASS);
-       locationComponent.tiltWhileTracking(45);
-    }
-
-    @Override
-    protected void onStart(){
-        super.onStart();
-        mapView.onStart();
-    }
-    @Override
-    protected void onResume(){
-        super.onResume();
-        mapView.onResume();
-    }
-    @Override
-    protected void onPause(){
-        super.onPause();
-        mapView.onPause();
-    }
-    @Override
-    protected void onStop(){
-        super.onStop();
-        mapView.onStop();
-    }
-    @Override
-    protected void onSaveInstanceState(Bundle outState){
-        super.onSaveInstanceState(outState);
-        mapView.onSaveInstanceState(outState);
-    }
-    @Override
-    public void onLowMemory(){
+    public void onLowMemory() {
         super.onLowMemory();
         mapView.onLowMemory();
     }
-    @Override
-    protected void onDestroy(){
-        super.onDestroy();
-        mapView.onDestroy();
-    } */
-
 }
+
