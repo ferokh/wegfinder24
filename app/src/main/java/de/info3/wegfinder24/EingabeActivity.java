8a1c55a5e889660e9f8b0143bde676e6c1502b20
gojo1021 <johannes.goetz99@gmail.com>
2022-12-08 15:06:35 +0100

    Karte wird angezeigt. Rest funktioniert nicht mehr wirklich

--- app/src/main/java/de/info3/wegfinder24/EingabeActivity.java
+++ app/src/main/java/de/info3/wegfinder24/EingabeActivity.java
@@ -1,5 +1,16 @@
 package de.info3.wegfinder24;
 
+import android.os.Bundle;
+
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
@@ -43,7 +54,95 @@ import java.util.ArrayList;
 
 public class EingabeActivity extends AppCompatActivity {
 
-    public final static String MAPBOX_TOKEN="pk.eyJ1IjoiYnJ1ZWNrdGkiLCJhIjoiY2xiYzBjNHI4MGI2NTNvcGV3OWI5aTJjciJ9.-ZRC7YTlnbSefuqY1u0TZg";
+    public final static String MAPBOX_TOKEN ="pk.eyJ1IjoiYnJ1ZWNrdGkiLCJhIjoiY2xiYzBjNHI4MGI2NTNvcGV3OWI5aTJjciJ9.-ZRC7YTlnbSefuqY1u0TZg";
+
+    private MapView mapView;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, MAPBOX_TOKEN);
+        setContentView(R.layout.activity_eingabe);
+
+        /*ImageButton btnOpenVariante = this.findViewById(R.id.btnArrow);
+
+         btnOpenVariante.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(EingabeActivity.this, WartebildschirmActivity.class);
+                startActivity(intent);
+            }
+        });*/
+
+
+
+        mapView = findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull MapboxMap mapboxMap) {
+                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+                    @Override
+                    public void onStyleLoaded(@NonNull Style style) {}
+                });
+            }
+
+        });
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+
+
+
+
+   /* public final static String MAPBOX_TOKEN="pk.eyJ1IjoiYnJ1ZWNrdGkiLCJhIjoiY2xiYzBjNHI4MGI2NTNvcGV3OWI5aTJjciJ9.-ZRC7YTlnbSefuqY1u0TZg";
 
     private LocationManager locationManager;
     MapView mapView;
@@ -71,6 +170,11 @@ public class EingabeActivity extends AppCompatActivity {
             }
         });
     }
+
+
+
+
+
 
 
     protected void initView(){
@@ -150,6 +254,6 @@ public class EingabeActivity extends AppCompatActivity {
     protected void onDestroy(){
         super.onDestroy();
         mapView.onDestroy();
-    }
+    } */
 
 }
