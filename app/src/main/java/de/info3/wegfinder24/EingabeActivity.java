c68d628fb79e754828aeae4a4ebb716f2e7d2006
gojo1021 <johannes.goetz99@gmail.com>
2022-12-15 15:39:57 +0100

    JG_Map finish

--- app/src/main/java/de/info3/wegfinder24/EingabeActivity.java
+++ app/src/main/java/de/info3/wegfinder24/EingabeActivity.java
@@ -56,9 +56,6 @@ public class EingabeActivity extends AppCompatActivity {
     private MyLocationNewOverlay locationOverlay;
 
 
-    //TODO: Layout überarbeiten, das Blau ist bisschen krass und die Form abändern könnte helfen
-    //TODO: Beim drücken des Knopfes wird die gleiche Activity wieder gestrartet. Wie das sein kann, keine Ahnung
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -137,7 +134,7 @@ public class EingabeActivity extends AppCompatActivity {
 
 
         //Marker setzen
-       /* GeoPoint startPoint=new GeoPoint(48.13,-1.63);
+        GeoPoint startPoint=new GeoPoint(48.13,-1.63);
         IMapController mapController = map.getController();
         mapController.setZoom(9);
         mapController.setCenter(startPoint);
@@ -145,7 +142,7 @@ public class EingabeActivity extends AppCompatActivity {
         Marker startMarker=new Marker(map);
         startMarker.setPosition(startPoint);
         startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
-        map.getOverlays().add(startMarker);*/
+        map.getOverlays().add(startMarker);
 
 
         //Polygon mit Array anzeigen lassen
@@ -226,154 +223,6 @@ public class EingabeActivity extends AppCompatActivity {
         //Configuration.getInstance().save(this, prefs);
         map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
     }
-}
-
-
-
-
-
-
-
-
-   /* ImageButton btnOpenVariante = this.findViewById(R.id.btnArrow);
-        btnOpenVariante.setOnClickListener(new View.OnClickListener()
-
-    {
-        @Override
-        public void onClick (View view){
-        Intent intent = new Intent(EingabeActivity.this, WartebildschirmActivity.class);
-        startActivity(intent);
-    }
-    });*/
-
-
-
-
-
-
-        /* //Kartenserver von Herr Knopf
-        String[] permissions = {
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                Manifest.permission.MANAGE_EXTERNAL_STORAGE
-        };
-
-        Permissions.check(this, permissions, null, null, new PermissionHandler() {  //Kontrolle der Berechtigungen
-            @Override
-            public void onGranted() {//wenn alles okay ist wird die Karte angezeigt
-                setupMapView();
-                String string = "Karte sofort erstellt";
-                Log.d("Karte",string);
-            }
-
-            @Override
-            public void onDenied(Context context, ArrayList<String> deniedPermissions) { //Wenn Berechtigung nicht okay ist, wird die Karte nicht angezeigt
-                super.onDenied(context, deniedPermissions);
-
-                if (deniedPermissions.size() == 1)
-                {
-                    String permission = deniedPermissions.get(0);
-                    if (permission.equals(Manifest.permission.MANAGE_EXTERNAL_STORAGE)) //darf auf den Speicher zugegriffen werden
-                    {
-                        setupMapView();
-                        String string = "Karte nicht sofort erstellt";
-                        Log.d("Karte", string);
-                    }
-                }
-            }
-        });
-
-        //Zugriff auf den Kartenserver der von Herr Knopf bereitgestellt wird
-        String authorizationString = this.getMapServerAuthorizationString("ws2223@hka", "LeevwBfDi#2027"); //username und passwort
-        Configuration.getInstance().getAdditionalHttpRequestProperties().put("Authorization", authorizationString);
-
-        XYTileSource mapServer = new XYTileSource("MapServer", 8, 20, 256, ".png", new String[]{"https://www.mapserver.dev/styles/default/"});
-
-        this.mapView = this.findViewById(R.id.mapView);
-        this.mapView.setTileSource(mapServer);
-
-        CompassOverlay compassOverlay = new CompassOverlay(this, mapView);
-        compassOverlay.enableCompass();
-        mapView.getOverlays().add(compassOverlay);
-
-        GeoPoint startPoint = new GeoPoint(48.8583, 2.2944);
-        IMapController mapController = mapView.getController();
-        mapController.setCenter(startPoint);
-        mapController.setZoom(9);
-//add
-        GpsMyLocationProvider provider = new GpsMyLocationProvider(this);
-        provider.addLocationSource(LocationManager.NETWORK_PROVIDER);
-        locationOverlay = new MyLocationNewOverlay(provider, mapView);
-        locationOverlay.enableFollowLocation();
-        locationOverlay.runOnFirstFix(new Runnable() {
-            public void run() {
-                Log.d("MyTag", String.format("First location fix: %s", locationOverlay.getLastFix()));
-            }
-        });
-        mapView.getOverlayManager().add(locationOverlay);
-
-
-    }
-    @SuppressLint("MissingPermission")
-    private void setupMapView()
-    {
-        //Abfrage GPS - Koordinaten des Handys
-        LocationListener locationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(android.location.Location location) {
-                double latitude = location.getLatitude();
-                double longitude = location.getLongitude();
-
-                GeoPoint startPoint = new GeoPoint(latitude, longitude);
-
-                IMapController mapController = mapView.getController();
-                mapController.setCenter(startPoint);
-            }
-
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String provider) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String provider) {
-
-            }
-        };
-
-        this.locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        this.locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 10, locationListener);
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
-
-        locationOverlay.enableMyLocation();
-        this.mapView.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        locationOverlay.disableMyLocation();
-        this.mapView.onPause();
-    }
-
-
-
-    private String getMapServerAuthorizationString(String username, String password)
-    {
-        String authorizationString = String.format("%s:%s", username, password);
-        return "Basic " + Base64.encodeToString(authorizationString.getBytes(StandardCharsets.UTF_8), Base64.DEFAULT);
-    }
 
     @Override
     protected void onDestroy() {
@@ -381,5 +230,4 @@ public class EingabeActivity extends AppCompatActivity {
         this.mapView.onPause();
     }
 
-
-}*/
+}
